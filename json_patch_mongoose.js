4da67fb89adf2506b4bf2795e505ec94fa62206f
Mark Gilcrease <mark@kr0ntech.com>
2024-04-26 21:03:10 -0500

    Removing mongoose Model.pull() call. It breaks things.

--- json_patch_mongoose.js
+++ json_patch_mongoose.js
@@ -109,12 +109,9 @@ class JSONPatchMongoose {
         let current_value = this.document.get(path);
         let parent = this.walkPath(path, -1);
         if(Array.isArray(parent)) {
-            if (!parent.pull) {
-                const i = parent.indexOf(current_value);
-                if (i !== -1)
-                    return parent.splice(parent.indexOf(current_value), 1);
-            }
-            return parent.pull(current_value);
+           const i = parent.indexOf(current_value);
+           if (i !== -1)
+						return parent.splice(parent.indexOf(current_value), 1);
         }
 
         this.setPath(path, undefined);
